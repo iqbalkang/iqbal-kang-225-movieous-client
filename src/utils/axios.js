aa05b5ed6c1814f7a830d8827454231b0b9db5f0
iqbal kang <iqbal-kang@iqbals-MacBook-Air.local>
2023-03-12 22:59:50 -0700

    fixed cast layout

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 const dev = 'http://127.0.0.1:8080/api/v1'
 const DOURL = 'https://movieous-server-2-3odfp.ondigitalocean.app/api/v1'
 const customFetch = axios.create({
-  baseURL: dev,
+  baseURL: DOURL,
 })
 
 export default customFetch
