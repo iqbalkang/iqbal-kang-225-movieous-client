dfa6e135d0b116235ec8ec30052a5ce123130f06
iqbal kang <iqbal-kang@iqbals-MacBook-Air.local>
2023-03-12 22:12:45 -0700

    axios url changed

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
-// const DOURL = 'https://movieous-server-2-3odfp.ondigitalocean.app/api/v1'
+const dev = 'http://127.0.0.1:8080/api/v1'
+const DOURL = 'https://movieous-server-2-3odfp.ondigitalocean.app/api/v1'
 const customFetch = axios.create({
-  baseURL: 'http://127.0.0.1:8080/api/v1',
+  baseURL: DOURL,
 })
 
 export default customFetch
