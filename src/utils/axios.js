b2e276ff4e46da60523f271f8e4aa08407cde5ce
iqbal kang <iqbal-kang@iqbals-MacBook-Air.local>
2023-03-23 17:56:33 -0700

    changed axios url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const dev = 'http://127.0.0.1:8080/api/v1'
-const prod = 'ec2-18-118-24-122.us-east-2.compute.amazonaws.com'
+const prod = 'ec2-18-218-202-165.us-east-2.compute.amazonaws.com'
 const customFetch = axios.create({
   baseURL: prod,
 })
