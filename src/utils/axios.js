e832f71b8904fa869a61d76ef3a6446df946b885
iqbal kang <iqbal-kang@iqbals-MacBook-Air.local>
2023-06-01 14:48:22 -0700

    added env file

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,8 +1,8 @@
-import axios from 'axios'
-const dev = 'http://127.0.0.1:8000/api/v1'
-const prod = 'ec2-18-218-202-165.us-east-2.compute.amazonaws.com'
-const customFetch = axios.create({
-  baseURL: dev,
-})
+import axios from 'axios';
+const link = process.env.REACT_APP_AXIOS_LINK;
 
-export default customFetch
+const customFetch = axios.create({
+  baseURL: link,
+});
+
+export default customFetch;
