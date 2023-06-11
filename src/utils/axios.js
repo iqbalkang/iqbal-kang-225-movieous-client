914c50df5e5fd944a7f13763e7620eba1ded5eec
iqbal kang <iqbal-kang@iqbals-MacBook-Air.local>
2023-06-10 18:57:03 -0700

    updated axios link

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const customFetch = axios.create({
   // baseURL: 'http://3.101.140.214/api/v1',
-  baseURL: 'http://127.0.0.1:8000/api/v1',
+  baseURL: 'http://54.219.114.236/api/v1',
 });
 
 export default customFetch;
